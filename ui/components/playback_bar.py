fc2290803780f4bbadb3799d66dff31f754171cb
Arham <arhamnoman17@gmail.com>
2026-08-14 22:42:12 +0500

    added play pause button toggle

--- ui/components/playback_bar.py
+++ ui/components/playback_bar.py
@@ -1,6 +1,6 @@
-from PySide6.QtWidgets import QWidget, QHBoxLayout, QLabel
-from PySide6.QtCore import Qt
-from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import QWidget, QHBoxLayout, QLabel, QPushButton
+from PySide6.QtCore import Qt, QSize
+from PySide6.QtGui import QIcon
 import os
 
 base_dir = os.path.dirname(os.path.abspath((os.path.join(__file__, ".."))))
@@ -10,19 +10,29 @@ icons_path = os.path.join(assets_path, "icons")
 class PlaybackBar(QWidget):
     def __init__(self, parent = None):
         super().__init__(parent)
+        self.is_playing = False
 
-        play_pixmap = QPixmap(os.path.join(icons_path, "play_icon.svg"))
-        self.play_pixmap = play_pixmap.scaled(20, 20, Qt.KeepAspectRatio, Qt.SmoothTransformation)
-        self.play_icon = QLabel()
-        self.play_icon.setPixmap(self.play_pixmap)
+        self.play_button = QPushButton()
+        self.play_button.setIcon(QIcon(os.path.join(icons_path, "play_icon.svg")))
+        self.play_button.setText("")
+        self.play_button.setFlat(True)
+        self.play_button.clicked.connect(self.toggle_play_pause)
 
         outer_layout = QHBoxLayout()
         control_layout = QHBoxLayout()
 
-        control_layout.addWidget(self.play_icon)
+        control_layout.addWidget(self.play_button)
         
         outer_layout.addStretch()
         outer_layout.addLayout(control_layout)
         outer_layout.addStretch()
 
         self.setLayout(outer_layout)
+
+    def toggle_play_pause(self):
+        self.is_playing = not self.is_playing
+
+        if self.is_playing:
+            self.play_button.setIcon(QIcon(os.path.join(icons_path, "pause_icon.svg")))
+        else:
+            self.play_button.setIcon(QIcon(os.path.join(icons_path, "play_icon.svg")))
